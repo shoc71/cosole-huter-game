03978773b1813c93b0bc97a1abff183fb73f7aba
Unos <129419169+shoc71@users.noreply.github.com>
2025-12-15 22:35:35 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+import random
+
+def play():
+    for x in range(5):
+        print(f"play : {x}")
+    pass
+
+def rules():
+    pass
+
+def map():
+    pass
+
+def goal():
+    # discover all the possible paths
+    # discover all the possible commands
+    # map out >90% of the maze
+    # slay all the monster(s)
+    # open the buried treasure
+    # survive
+    pass
+    
+'''
+ok but why would anyone stare at an empty screen and type out fixed options with limited exploration?
+
+Is this secretly a horror game? What reason do they have to be invested in this?
+
+'''
+
+POSSIBLE_COMMANDS = {
+    "play" : play(),
+    "rules" : rules(),
+    "goal" : goal(),
+}
+
+def user_input():
+    pass
+
+def sanitize_input():
+    pass
+
+def gameloop():
+    pass
+
+def command_search():
+    pass
+
+def string_search():
+    pass
+
+def list_search():
+    pass
+
